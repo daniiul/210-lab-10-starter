7f8061c967a08f98f6872204348f0817bff470f6
daniiul <dmalakhov2005@gmail.com>
2024-10-03 18:03:18 -0700

    initial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+// COMSC-210 | Lab 12 | Justin Beiber
+//IDE used: codeblocks
 #include <iostream>
 using namespace std;
 
 int main() {
 
-    // add any code here, sort of a 'hello world plus' code block
+    cout << "Hello World!" << endl;
 
     return 0;
 }
